4b5db162b31626be7f3a15f0153b385599b2b08d
Ellis <sc22es@leeds.ac.uk>
2024-09-12 17:16:48 +0100

    Added Leaderboard

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
 
 using namespace std;
 
@@ -7,10 +10,10 @@ using namespace std;
 class pegBoard {
 
     private:
-        static const char colours[]; // static means all the INSTANCES share this one variables
         char pegs[4];
 
     public:
+        static const char colours[8]; // static means all the INSTANCES share this one variables
 
         pegBoard() {
             pegs[0] = colours[rand() % 8];
@@ -20,6 +23,28 @@ class pegBoard {
         }
 
         bool guess(string g) {
+
+            int count=0;
+
+            for (char ele : colours) {
+                if (g[0] == ele) {
+                    count++;
+                }
+                if (g[1] == ele) {
+                    count++;
+                }
+                if (g[2] == ele) {
+                    count++;
+                }
+                if (g[3] == ele) {
+                    count++;
+                }
+            }
+
+            if (count != 4) {
+                cout << "Error! Invalid guess\n";
+                return false;
+            }
 
             int X=0, O=0;
 
@@ -64,16 +89,20 @@ class pegBoard {
         }
 
         void printPegs() {
-            cout << "pegs: " << pegs << endl;
+            cout << "Answer: " << pegs << endl;
         }
 
 };
 
-const char pegBoard::colours[] = {'R', 'G', 'B', 'C', 'M', 'Y', 'B', 'W'};
+const char pegBoard::colours[8] = {'R', 'G', 'B', 'C', 'M', 'Y', 'K', 'W'};
 
 
 
 int main() {
+
+    cout << "Enter Player Name: ";
+    string name;
+    getline(cin, name);
 
     srand(time(NULL));
 
@@ -81,18 +110,34 @@ int main() {
 
     string pegGuess;
 
-    int round=1;
+    cout << "\nPeg colours availible:  ";
+    for (char colour : board->colours) {
+        cout << colour << ", ";
+    }
+    cout << endl;
 
+    board->printPegs();
+
+    int round=0;
     do {
-        cout << "Round " << round++ << ": " << endl;
+        cout << "Round " << ++round << ": " << endl;
         cin >> pegGuess;
     }
     while (!board->guess(pegGuess));
 
-
     board->printPegs();
 
     delete board;
+
+    time_t currentTime = time(NULL);
+
+    ofstream file("leaderboard.txt", ios::app);
+
+    file << left << setw(20) << name.substr(0, 20) << " | ";
+    file << right << setw(5) << round << " | ";
+    file << ctime(&currentTime);
+
+    file.close();
 
     return 0;
 }
